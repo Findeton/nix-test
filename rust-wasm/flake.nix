1eaa775f8e398e22846329fb81b79b3299b08cf6
Findeton <felrobelv@gmail.com>
2022-07-04 09:15:32 +0100

    wip

--- rust-wasm/flake.nix
+++ rust-wasm/flake.nix
@@ -1,48 +1,41 @@
 {
-  description = "Flake for rust-wasm";
+  description = "Minimal rust wasm32-unknown-unknown example";
 
-  # input
   inputs.rust-overlay.url = "github:oxalica/rust-overlay";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  # output function of this flake
+  
+# x86_64-pc-linux-gnu
   outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-        let 
-          overlays = [ (import rust-overlay) ];
-          # pkgs is just the nix packages
-          pkgs = import nixpkgs {
-            inherit system overlays;
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { 
+          inherit system overlays;
+          crossSystem = {
+            config = "wasm32-unknown-unknown";
+            useLLVM = true;
           };
-          rust-nightly = pkgs
-            .rust-bin
-            .selectLatestNightlyWith(
-              toolchain: toolchain.default.override {
-                extensions = [ "rust-src" ];
-                targets = [ "wasm32-unknown-unknown" ];
-              }
-            );
-
-        # resulting packages of the flake
-        in rec {
-          # WIP Derivation for rust-wasm
-          # Continue work here following https://srid.ca/rust-nix reference
-          packages.rust-wasm = pkgs.clangStdenv.mkDerivation {
-            name = "rust-wasm";
-            version = "0.0.1";
-            src = self;
-            type = "git"; 
-            submodules = "true";
-            nativeBuildInputs = [
-                rust-nightly
-                pkgs.wasm-pack
-                pkgs.wasm-bindgen-cli
-                pkgs.libiconv
-            ];
+        };
+        rust-nightly = pkgs
+          .rust-bin
+          .selectLatestNightlyWith(
+            toolchain: toolchain.default.override {
+              extensions = [ "rust-src" ];
+              targets = [ "wasm32-unknown-unknown" ];
+            }
+        );
+      in rec {
+        defaultPackage = pkgs.rustPlatform.buildRustPackage {
+          pname = "rust-wasm";
+          version = "0.0.1";
+          src = ./.;
+          nativeBuildInputs = [
+            rust-nightly
+            pkgs.wasm-pack
+            pkgs.wasm-bindgen-cli
+          ];
           buildPhase = ''
-            cd rust-wasm
             echo 'Build: flags'
             export RUSTFLAGS='-C target-feature=+atomics,+bulk-memory,+mutable-globals'
             echo 'Build: wasm-pack build'
@@ -50,19 +43,11 @@
             echo 'Build: wasm-pack pack'
             wasm-pack pack .
           '';
-          installPhase = "echo 'Skipping installPhase'";
-          };
-          # rust-wasm is the default package
-          defaultPackage = packages.rust-wasm;
 
-          # configure the dev shell
-          devShell = (
-            pkgs.mkShell.override { stdenv = pkgs.clangStdenv; }
-          ) { 
-            buildInputs = 
-              packages.rust-wasm.nativeBuildInputs ++
-              [ pkgs.bash ]; 
+          cargoLock = {
+            lockFile = ./Cargo.lock;
           };
-        }
+        };
+      }
     );
 }
